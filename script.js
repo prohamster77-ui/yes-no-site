b44610dd58fb2827fc9f1ef5d4b8e913a3db00ed
prohamster77-ui <prohamster77@gmail.com>
2026-01-30 20:54:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ let messages = [
   "If you say no, I will be really sad...",
   "Please reconsider 😢",
   "I’m begging you...",
-  "Last chance..."
+  "Last chance...",
+  "Pleasee speed i need thissss",
 ];
 
 let messageIndex = 0;
@@ -22,5 +23,29 @@ function handleNoClick() {
 }
 
 function handleYesClick() {
-  window.location.href = "yes.html";
+  fetch("https://formspree.io/f/xeekrndr", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      answer: "YES clicked",
+      time: new Date().toLocaleString()
+    })
+  });
+
+  document.body.innerHTML = `
+    <div style="
+      height:100vh;
+      display:flex;
+      flex-direction:column;
+      justify-content:center;
+      align-items:center;
+      text-align:center;
+      font-family:Arial;
+    ">
+      <h1>REALLY??!?!! I LOVE YOUUUU 💗</h1>
+      <img src="https://media.tenor.com/HKQ9w2VpTHwAAAAj/ily.gif" style="width:250px;margin-top:20px;">
+    </div>
+  `;
 }
